298e8dda7343d6deb12726cdbf4256a6eea82b4e
Chandanababu98 <chandanababu98@gmail.com>
2025-03-24 13:44:57 +0530

    ForgotPasswordTest_FP-1

---
+++ src/main/java/objectRepository/ForgotPassword_Obj.java
@@ -0,0 +1,9 @@
+package objectRepository;
+
+import org.openqa.selenium.By;
+
+public class ForgotPassword_Obj {
+	
+	//Ele
+	public By Ele_ResetPasswordHeading(String text) {return By.xpath("//b[text()='"+text+"']");}
+}
