32788cedf1b3ce79ca3e03c1285a72bcc235a17a
Annu-James <annamaria1551@gmail.com>
2025-03-24 15:52:09 +0530

    CodeChange

--- src/main/java/TestData/CommonData.java
+++ src/main/java/TestData/CommonData.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 public class CommonData {
 	//GenericData
-	//public static String UserName = "neethumahendran5@gmail.com";
-	//public static String PassWord = "Tigerscott@123";
+	public static String UserName = "neethumahendran5@gmail.com";
+	public static String PassWord = "Tigerscott@123";
 	
-	public static String UserName = "jhoncy.j@se-mentor.com";
-	public static String PassWord = "Jhoncy@123";
+	//public static String UserName = "jhoncy.j@se-mentor.com";
+	//public static String PassWord = "Jhoncy@123";
 
 	
 }
