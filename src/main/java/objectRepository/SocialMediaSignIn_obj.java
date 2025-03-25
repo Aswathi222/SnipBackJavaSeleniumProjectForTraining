9dfc4007414a5021a7673f80080f936cbfa0a20a
Annu-James <annamaria1551@gmail.com>
2025-03-25 11:29:33 +0530

    Social Media sign in TC01 and TC02 modified

--- src/main/java/objectRepository/SocialMediaSignIn_obj.java
+++ src/main/java/objectRepository/SocialMediaSignIn_obj.java
@@ -5,5 +5,4 @@ import org.openqa.selenium.By;
 public class SocialMediaSignIn_obj {
 	//Btn	
 		public By Btn_Login(String text) {return By.xpath("//a[text()='"+text+"']");}
-		public By Btn_SignInWithGoogle(String text) {return By.xpath("//a[@href='https://web06-stg-ch.snipback.com/login/google']");}
 }
--- src/main/java/testPage/MavenPageObject/SocialMediaSignIn.java
+++ src/main/java/testPage/MavenPageObject/SocialMediaSignIn.java
@@ -2,7 +2,6 @@ package testPage.MavenPageObject;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-
 import objectRepository.LoginPage_Obj;
 import objectRepository.SocialMediaSignIn_obj;
 import utilPack.BasePge;
@@ -17,26 +16,27 @@ public class SocialMediaSignIn extends BasePge{
 	}
 
 	//<summary>
-	//Test Case Title:"To verify there is a option to Sign In with Google in login page."
+	//Test Case Title:"To verify there is a option to 'Sign In with Google' in login page."
 	//Automation ID: TC_01
 	//</summary>
-	
 	public void TC_01SocialMediaSignIn() throws InterruptedException{
-		
 		LoginPage_Obj LoginPageObj=new LoginPage_Obj();
 		SocialMediaSignIn_obj SocialMediaSignInObj=new SocialMediaSignIn_obj();
 		
 		//step 1  : Navigate to Snipback Website
-		//Expected: User should be able to navigate to Snipback website
+		//Expected: User should navigate to Login page
 		base.buttonClick(LoginPageObj.Btn_Login("Login"));
-		asrt.assertTrue(base.isExists(LoginPageObj.Btn_Login("Login")),"User unable to Login");
+		asrt.assertTrue(base.isExists(LoginPageObj.Btn_Login("Login")),"User is unable to login the website");
 		
+		//step 2  : To verify there is an option 'Sign in with google' in login page
+	    //Expected: There should be a option to Sign In with Google in login page
+		asrt.assertTrue(base.isExists(SocialMediaSignInObj.Btn_Login(" Sign In with Google")),"User is unable to view the Sign in with google button in the login page");	
 	}
+	
 	//<summary>
 	//Test Case Title:"To verify what happens when user clicks already logged in Google account."
 	//Automation ID: TC_02
 	//</summary>
-	
 	public void TC_02SocialMediaSignIn() throws InterruptedException{
 		LoginPage_Obj LoginPageObj=new LoginPage_Obj();
 		SocialMediaSignIn_obj SocialMediaSignInObj=new SocialMediaSignIn_obj();
@@ -44,12 +44,10 @@ public class SocialMediaSignIn extends BasePge{
 		//step 1:Navigate to Snipback Website
 		//Expected: User should be able to navigate to Snipback website
 		base.buttonClick(LoginPageObj.Btn_Login("Login"));
-		asrt.assertTrue(base.isExists(LoginPageObj.Btn_Login("Login")),"User unable to Login");
+		asrt.assertTrue(base.isExists(LoginPageObj.Btn_Login("Login")),"User is unable to login the website");
 		
-		//step 2: Click Sign In with Google
+		//step 2: User should able to click Sign In with Google
 		//Expected: User able to click Sign in with Google button.
-		base.buttonClick(SocialMediaSignInObj.Btn_SignInWithGoogle(" Sign In with Google"));
-		//asrt.assertTrue(base.isExists(SocialMediaSignInObj.Btn_SignInWithGoogle(" Sign In with Google")), "User unable to Sign in with Google");
-		//Thread.sleep(2500);
+		base.buttonClick(SocialMediaSignInObj.Btn_Login(" Sign In with Google"));
 	}
 }
