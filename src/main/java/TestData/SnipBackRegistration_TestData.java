0f0ba61ce64cc7a23bbbddfb304925c158eb1494
SsangeethaM <msangeetha9187@gmail.com>
2025-03-25 11:14:05 +0530

    Resolved the issues in TC01_Registration

--- src/main/java/TestData/SnipBackRegistration_TestData.java
+++ src/main/java/TestData/SnipBackRegistration_TestData.java
@@ -1,17 +1,11 @@
 package TestData;
 
-public class SnipBackRegistration_TestData {
-
+public class SnipBackRegistration_TestData 
+{
 	// TC01_Registration
-	public static String TC01_RegistrationInvalidName="   ";
-		
+	public static String TC01_RegistrationInvalidName="   ";	
 	public static String TC01_RegistrationInvalidCharName="Tintu89";
-	
-	
 	public static String TC01_RegistrationValidName="TINTU";
-
-	
-
 }
 
 
--- src/main/java/testPage/MavenPageObject/SnipBackRegistration.java
+++ src/main/java/testPage/MavenPageObject/SnipBackRegistration.java
@@ -21,42 +21,43 @@ public class SnipBackRegistration extends BasePge{
 	}
 	// <summary>
 	// Test Case Title :    "1.Verify if the name field is mandatory or shows an error message when left blank.
-	                     //  2. Verify if the field shows error message for numbers and special characters entry
-	                     //  3.Verify if the name field uses only alphabets"
+	//  2. Verify if the field shows error message for numbers and special characters entry
+	//  3.Verify if the name field uses only alphabets"
 	// Automation ID :TC01_Registration
 	// </summary>
+	
 	public void TC01_Registration() 
 	{
 		Registration_Obj   RegistrationObj = new Registration_Obj();
 		SnipBackRegistration_TestData  snipbackregistrationtestdata = new SnipBackRegistration_TestData();
-		
+
 		//Step 1 :  Click on Login button
 		//Expected : User should click on login button
 		base.buttonClick(RegistrationObj.Btn_Login("nav-link btn btn-white sm", "Login"));
 		asrt.assertTrue(base.isExists(RegistrationObj.Btn_SignUp("signup-btn","Sign Up")),"User unable to find sign up button.");
-		
+
 		//Step 2: Click on sign up button
 		//Expected : User should click on signup button
 		base.buttonClick(RegistrationObj.Btn_SignUp("signup-btn","Sign Up"));
-		
+		asrt.assertTrue(base.isExists(RegistrationObj.Ele_Name("form-inlines","first_name")),"User is unable to click on signup button");		
+
 		//Step 3 : Verify if the name field is mandatory or shows an error message when left blank.
 		//Expected : There should be a pop-up message "Please enter ur name"
 		base.setData(RegistrationObj.Ele_Name("form-inlines","first_name"), SnipBackRegistration_TestData.TC01_RegistrationInvalidName);
 		base.buttonClick(RegistrationObj.Btn_SigUp1("btn-signup"));
-		asrt.assertTrue(base.isExists(RegistrationObj.Btn_SigUp1("btn-signup")),"User unable to find a pop up meggase.");
-		
-		
+		asrt.assertTrue(base.isExists(RegistrationObj.Btn_SigUp1("btn-signup")),"User is unable to find the pop up message as 'Please enter your name'.");
+
 		//Step 4 : Verify if the field shows error message for numbers and special characters entry
 		//Expected : There should be a pop-up message "Name field must only contain letters "
 		base.setData(RegistrationObj.Ele_Name("form-inlines","first_name"),SnipBackRegistration_TestData.TC01_RegistrationInvalidCharName);
 		base.buttonClick(RegistrationObj.Btn_SigUp1("btn-signup"));
-		asrt.assertTrue(base.isExists(RegistrationObj.Btn_SigUp1("btn-signup")),"User unable to enter name with numbers and special characters  ");
-		
-		
+		asrt.assertTrue(base.isExists(RegistrationObj.Btn_SigUp1("btn-signup")),"User is unable to find the pop up message as 'Name field must only contain letters'.");
+
 		//Step5 : Verify if the name field uses only alphabets
 		//Expected : It should accept mentioned test data.
 		base.setData(RegistrationObj.Ele_Name("form-inlines","first_name"),SnipBackRegistration_TestData.TC01_RegistrationValidName);
 		base.buttonClick(RegistrationObj.Btn_SigUp1("btn-signup"));	
+		asrt.assertTrue(base.isExists(RegistrationObj.Ele_Emailerror("err_email")),"User is unable to accept the mentioned test data");		
 	}
 }
 
