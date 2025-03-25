7e92f62c2306d42b9da8322d89080c8bc6214f88
Annu-James <annamaria1551@gmail.com>
2025-03-25 15:56:41 +0530

    TC01,TC02 Social media commit

--- src/main/java/objectRepository/SocialMediaSignIn_obj.java
+++ src/main/java/objectRepository/SocialMediaSignIn_obj.java
@@ -4,5 +4,5 @@ import org.openqa.selenium.By;
 
 public class SocialMediaSignIn_obj {
 	//Btn	
-		public By Btn_Login(String text) {return By.xpath("//a[text()='"+text+"']");}
+		public By Btn_Login(String text) {return By.xpath("//a[normalize-space(text()='"+text+"']");}
 }
--- src/main/java/testPage/MavenPageObject/SocialMediaSignIn.java
+++ src/main/java/testPage/MavenPageObject/SocialMediaSignIn.java
@@ -26,7 +26,7 @@ public class SocialMediaSignIn extends BasePge{
 		//step 1  : Navigate to Snipback Website
 		//Expected: There should be a option to view 'Sign In with Google' in login page
 		base.buttonClick(LoginPageObj.Btn_Login("Login"));
-		asrt.assertTrue(base.isExists(SocialMediaSignInObj.Btn_Login(" Sign In with Google")),"User is unable to view the Sign in with google button in the login page");	
+		asrt.assertTrue(base.isExists(SocialMediaSignInObj.Btn_Login("Sign In with Google")),"User is unable to view the Sign in with google button in the login page");	
 	}
 	
 	//<summary>
@@ -40,10 +40,13 @@ public class SocialMediaSignIn extends BasePge{
 		//step 1:Navigate to Snipback Website
 		//Expected: There should be an option to view 'Sign In with Google' in login page.
 		base.buttonClick(LoginPageObj.Btn_Login("Login"));
-		asrt.assertTrue(base.isExists(SocialMediaSignInObj.Btn_Login(" Sign In with Google")),"User is unable to view the Sign in with google button in the login page");
+		asrt.assertTrue(base.isExists(SocialMediaSignInObj.Btn_Login("Sign In with Google")),"User is unable to view the Sign in with google button in the login page");
 		
 		//step 2: User should able to click Sign In with Google
-		//Expected: User able to click Sign in with Google button.
-		base.buttonClick(SocialMediaSignInObj.Btn_Login(" Sign In with Google"));
+		//Expected: The user should navigate to Snipback film page.
+		String title1=driver.getCurrentUrl();
+		base.buttonClick(SocialMediaSignInObj.Btn_Login("Sign In with Google"));
+		String title2=driver.getCurrentUrl();
+		asrt.assertNotEquals(title1, title2,"The user is unable to navigate to Snipback film page. ");
 	}
 }
