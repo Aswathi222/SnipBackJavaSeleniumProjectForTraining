56bd8731ebdf466a2a9afae0cfa6c38894da640f
SsangeethaM <msangeetha9187@gmail.com>
2025-03-27 16:01:48 +0530

    TC03_Registration PR Created

--- src/main/java/TestData/SnipBackRegistration_TestData.java
+++ src/main/java/TestData/SnipBackRegistration_TestData.java
@@ -16,6 +16,16 @@ public class SnipBackRegistration_TestData
 	public static String TC02_RegistrationValidUsername="MANASI";
 	public static String TC02_RegistrationPassword="Tigerscott@123";
 	public static String TC02_RegistrationValidName="TINTU";
+
+	//TC03_Registration
+	public static String TC03_RegistrationUsername="tintu<>";
+	public static String TC03_RegistrationExeedUsername="qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvb2345eyegbdbhhjd";
+	public static String TC03_RegistrationValidUsername="angle$88#";
+	public static String TC03_registrationvalidemail="neethumahendran@gmail.com";
+	public static String TC03_RegistrationValidName="TINTU";
+	public static String TC03_RegistrationPassword="Tigerscott@123";
+	public static String TC03_RegistrationTrimName="TINTU TOM";
+	
 }
 
 
