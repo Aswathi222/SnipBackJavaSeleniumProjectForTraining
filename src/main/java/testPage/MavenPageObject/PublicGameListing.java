d9f8d69694cb2b5c84e8248791c6c8c287311278
Benson Biju <94179666+LeviAckerman22@users.noreply.github.com>
2025-04-02 14:49:09 +0530

    PR CREATED FOR PublicGameListing_05

--- src/main/java/testPage/MavenPageObject/PublicGameListing.java
+++ src/main/java/testPage/MavenPageObject/PublicGameListing.java
@@ -101,6 +101,28 @@ public class PublicGameListing extends BasePge{
 			asrt.assertTrue(base.isExists(LoginPageObj.Edt_AlertText("PublicGameLisitngTestGame")) &&  base.isExists(PublicGameObj.Ele_Date("Friday,", " Mar 28 2025")), "User is unable to see public game 'PublicGameLisitngTestGame' with date 'Friday,  Mar 28 2025' on the Films page");
 		   
 		}
-	
+		//<summary>
+		//Verify that the user should be able to see the public games as per the recent dates along with day after entering into FILM page
+	    //Automation ID:GameListing_05
+		//</summary>
+		public void GameListing_05_PublicGameListing() throws InterruptedException {
+			LoginPage_Obj LoginPageObj = new LoginPage_Obj();
+			PublicGameListing_Obj PublicGameObj=new PublicGameListing_Obj();
+			
+			// Step1: User navigated to SnipBack Website after entering snipback url
+			// Expected:The user should be able to navigate to the  Snipback website after entering the URL
+			asrt.assertTrue(base.isExists(LoginPageObj.Ele_SnipBackHomePageLogo("light-logo")), "User is unable to navigate to SnipBack website after entering the URL");
+			
+			//Step2:Click on FILM Tab without login
+			//Expected:The user should be able to click on "FILM" tab without login to snipback
+			base.buttonClick(PublicGameObj.Btn_Home("Film","menu_link nav-link "));
+			asrt.assertTrue(base.isExists(LoginPageObj.Edt_Alert1(" Amplifies")),"User is unable to click on Films tab without login to SnipBack");	
+			
+			//Step 3  :"Verify user able to see the public games as per the condition"
+			//Expected:"User should be able to see the public games as per the recent dates along with day(eg: Wednesday, Oct 23 2024) after entering into FILM page" 
+			asrt.assertTrue(base.isExists(PublicGameObj.Btn_Sort("myCheckbox1")),"User is unable to see the 'Sort by Date' Up Arrow button on the Film page of Snipback.");
+			base.buttonClick(PublicGameObj.Btn_Sort("myCheckbox1"));
+			asrt.assertTrue(base.isExists(LoginPageObj.Edt_AlertText("PublicGameLisitngTestGame")) &&  base.isExists(PublicGameObj.Ele_Date("Friday,", " Mar 28 2025")), "User is unable to see public games as per the recent dates.");
+		}
 		
 }
