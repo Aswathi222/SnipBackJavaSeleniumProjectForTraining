a5f044d8bd7180acf0c1fa42835d14f50483b276
Chandanababu98 <chandanababu98@gmail.com>
2025-04-07 10:15:33 +0530

    WE01 CreateAndAddNewMemberWithOrWithoutEmail Issue Resolved

--- src/main/java/TestData/CreateAndAddNewMemberWithOrWithoutEmail_TestData.java
+++ src/main/java/TestData/CreateAndAddNewMemberWithOrWithoutEmail_TestData.java
@@ -4,6 +4,8 @@ public class CreateAndAddNewMemberWithOrWithoutEmail_TestData {
 
 	// WE01_CreateandAddnewmemberwithorwithoutEmail
 	public static String WE01_CreateandAddnewmemberOrganization="demo organization";
+	// WE01_CreateandAddnewmemberwithorwithoutEmail
+	public static String WE03_CreateandAddnewmemberOrganization="test123@gmail.com";
 }
 
 
