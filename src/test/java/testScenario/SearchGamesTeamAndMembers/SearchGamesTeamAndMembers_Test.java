7be4cb8a9061cb81ce4c5a9db0a4523801b7e257
Annu-James <annamaria1551@gmail.com>
2025-04-08 11:32:21 +0530

    SH05_SearchGameTeamAndMembers

--- src/test/java/testScenario/SearchGamesTeamAndMembers/SearchGamesTeamAndMembers_Test.java
+++ src/test/java/testScenario/SearchGamesTeamAndMembers/SearchGamesTeamAndMembers_Test.java
@@ -9,33 +9,39 @@ import utilPack.BaseTest;
 public class SearchGamesTeamAndMembers_Test extends BaseTest 
 {	
 	@Test()
-	public void SH01_SearchGamesTeamAndMembers() throws Exception 
+	public void SH01_SearchGamesTeamAndMembersTest() throws Exception 
 	{	
 		SearchGamesTeamAndMembers searchGamesTeamAndMembersTest = new SearchGamesTeamAndMembers(getDriver());
 		searchGamesTeamAndMembersTest.SH01_SearchGamesTeamAndMembers();		
 	}	
 	@Test()
-	public void SH02_SearchGamesTeamAndMembers() throws Exception 
+	public void SH02_SearchGamesTeamAndMembersTest() throws Exception 
 	{	
 		SearchGamesTeamAndMembers searchGamesTeamAndMembersTest = new SearchGamesTeamAndMembers(getDriver());
 		searchGamesTeamAndMembersTest.SH02_SearchGamesTeamAndMembers();		
 	}	
 	@Test()
-	public void SH03_SearchGamesTeamAndMembers() throws Exception 
+	public void SH03_SearchGamesTeamAndMembersTest() throws Exception 
 	{	
 		SearchGamesTeamAndMembers searchGamesTeamAndMembersTest = new SearchGamesTeamAndMembers(getDriver());
 		searchGamesTeamAndMembersTest.SH03_SearchGamesTeamAndMembers();		
 	}	
 	@Test()
-	public void SH04_SearchGamesTeamAndMembers() throws Exception 
+	public void SH04_SearchGamesTeamAndMembersTest() throws Exception 
 	{	
 		SearchGamesTeamAndMembers searchGamesTeamAndMembersTest = new SearchGamesTeamAndMembers(getDriver());
 		searchGamesTeamAndMembersTest.SH04_SearchGamesTeamAndMembers();		
 	}
 	@Test()
-	public void SH05_SearchGamesTeamAndMembers() throws Exception 
+	public void SH05_SearchGamesTeamAndMembersTest() throws Exception 
 	{	
 		SearchGamesTeamAndMembers searchGamesTeamAndMembersTest = new SearchGamesTeamAndMembers(getDriver());
 		searchGamesTeamAndMembersTest.SH05_SearchGamesTeamAndMembers();		
+	}
+	@Test()
+	public void SH13_SearchGamesTeamAndMembersTest() throws Exception 
+	{	
+		SearchGamesTeamAndMembers searchGamesTeamAndMembersTest = new SearchGamesTeamAndMembers(getDriver());
+		searchGamesTeamAndMembersTest.SH13_SearchGamesTeamAndMembers();		
 	}
 }
