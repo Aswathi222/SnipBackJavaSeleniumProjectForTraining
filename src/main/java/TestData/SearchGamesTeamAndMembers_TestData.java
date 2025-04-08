e2a08ba048c12c1e09389ff772d28965f9fbf6e0
Annu-James <annamaria1551@gmail.com>
2025-04-08 11:45:04 +0530

    SH_05_SearchGameTeamAndMembers

--- src/main/java/TestData/SearchGamesTeamAndMembers_TestData.java
+++ src/main/java/TestData/SearchGamesTeamAndMembers_TestData.java
@@ -3,14 +3,15 @@ package TestData;
 public class SearchGamesTeamAndMembers_TestData {
 	
 	//SH02_SearchGamesTeamAndMembers
-	public static String SH02_SearchGamesTeamAndMembers="T";
+	public static String SH_02_SearchGamesTeamAndMembers="T";
 	
 	//SH03_SearchGamesTeamAndMembers
-	public static String SH03_SearchGamesTeamAndMembers_Number="1";
+	public static String SH_03_SearchGamesTeamAndMembers_Number="1";
 	
-	//SH04_SearchGamesTeamAndMembers
-	public static String SH04_SearchGamesTeamAndMembers_Special="@";
+	//SH_04_SearchGamesTeamAndMembers
+	public static String SH_04_SearchGamesTeamAndMembers_Special="@";
 
-	//SH05_SearchGamesTeamAndMembers
-	public static String SH05_SearchGamesTeamAndMembers="I'm not here";
+	//SH_05_SearchGamesTeamAndMembers
+	public static String SH_05_SearchGamesTeamAndMembers="I'm not here";
+
 }
