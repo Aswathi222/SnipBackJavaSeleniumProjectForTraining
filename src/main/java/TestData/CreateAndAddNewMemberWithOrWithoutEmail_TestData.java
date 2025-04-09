ddd55e76485242707ef6b4d7ac9abdf55a909206
Chandanababu98 <chandanababu98@gmail.com>
2025-04-09 14:57:52 +0530

    WE_07_CreateAndAddNewMemberWithOrWithoutEmail PR Created

--- src/main/java/TestData/CreateAndAddNewMemberWithOrWithoutEmail_TestData.java
+++ src/main/java/TestData/CreateAndAddNewMemberWithOrWithoutEmail_TestData.java
@@ -5,15 +5,34 @@ public class CreateAndAddNewMemberWithOrWithoutEmail_TestData {
 	// WE01_CreateandAddnewmemberwithorwithoutEmail
 	public static String WE_01_CreateandAddnewmemberOrganization="demo organization";
 	public static String WE_01_CreateandAddnewmemberTeam="475 Playmakers";
+	// WE02_CreateandAddnewmemberwithorwithoutEmail
+	public static String WE_02_CreateandAddnewmemberOrganization="demo organization";
+	public static String WE_02_CreateandAddnewmemberTeam="475 Playmakers";
 	// WE03_CreateandAddnewmemberwithorwithoutEmail
+	public static String WE_03_CreateandAddnewmemberOrganization="demo organization";
+	public static String WE_03_CreateandAddnewmemberTeam="475 Playmakers";
 	public static String WE_03_CreateandAddnewmemberEmail="test123@gmail.com";
 	// WE0_4_CreateandAddnewmemberwithorwithoutEmail
+	public static String WE_04_CreateandAddnewmemberOrganization="demo organization";
+	public static String WE_04_CreateandAddnewmemberTeam="475 Playmakers";
 	public static String WE_04_CreateandAddnewmemberEmail="test123gmail.com";
 	// WE05_CreateandAddnewmemberwithorwithoutEmail
+	public static String WE_05_CreateandAddnewmemberOrganization="demo organization";
+	public static String WE_05_CreateandAddnewmemberTeam="475 Playmakers";
 	public static String WE_05_CreateandAddnewmemberFirstName="New";
 	public static String WE_05_CreateandAddnewmemberLastName="User";
 	public static String WE_05_CreateandAddnewmemberJerseyNumber="123";
 	public static String WE_05_CreateandAddnewmemberMessage="Hi New User! Welcome to the Team";
+	// WE06_CreateandAddnewmemberwithorwithoutEmail
+	public static String WE_06_CreateandAddnewmemberOrganization="demo organization";
+	public static String WE_06_CreateandAddnewmemberTeam="475 Playmakers";
+	public static String WE_06_CreateandAddnewmemberEmail="test123@gmail.com";
+	public static String WE_06_CreateandAddnewmemberFirstName="New";
+	// WE07_CreateandAddnewmemberwithorwithoutEmail
+	public static String WE_07_CreateandAddnewmemberOrganization="demo organization";
+	public static String WE_07_CreateandAddnewmemberTeam="475 Playmakers";
+	public static String WE_07_CreateandAddnewmemberEmail="test123@gmail.com";
+	public static String WE_07_CreateandAddnewmemberInvalidFirstName="N";
 }
 
 
