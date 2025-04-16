f8fe97f8ad390f314b04e3778bfe8dd79dcecf0b
Benson Biju <94179666+LeviAckerman22@users.noreply.github.com>
2025-04-16 13:42:15 +0530

    PR CREATED FOR PublicGameListing_15

--- src/test/java/testScenario/PublicGameListing/PublicGameListing_Test.java
+++ src/test/java/testScenario/PublicGameListing/PublicGameListing_Test.java
@@ -90,5 +90,11 @@ public class PublicGameListing_Test extends BaseTest
 		PublicGameListing publicGameListing=new PublicGameListing(getDriver());
 		publicGameListing.GameListing_14_PublicGameListing();	
 	}
+	@Test()
+	public void GameListing_15_PublicGameListingTest() throws Exception 
+	{		
+		PublicGameListing publicGameListing=new PublicGameListing(getDriver());
+		publicGameListing.GameListing_15_PublicGameListing();	
+	}
 }
 
