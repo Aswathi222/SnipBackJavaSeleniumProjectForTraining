b83142207114692db3e8c65d78361f4fa2517e26
Chandanababu98 <chandanababu98@gmail.com>
2025-04-16 15:55:25 +0530

    WE_14_CreateAndAddNewMemberWithOrWithoutEmail PR Created

--- src/main/java/objectRepository/CreateAndAddNewMemberWithOrWithoutEmail_Obj.java
+++ src/main/java/objectRepository/CreateAndAddNewMemberWithOrWithoutEmail_Obj.java
@@ -11,8 +11,10 @@ public class CreateAndAddNewMemberWithOrWithoutEmail_Obj {
 	//Ele	
 	public By Ele_list(String Ele_list_id1, String Ele_list_id2) {return By.xpath("//div[@id='"+Ele_list_id1+"']//div[@id='"+Ele_list_id2+"']");}
 	public By Ele_SearchGame(String Edt_TeamNameClass) {return By.xpath("//input[@class='"+Edt_TeamNameClass+"']");}
+	public By Ele_Error(String ErrorText) {return By.xpath("//li[text()=\""+ErrorText+"\"]");}
 	//Edt
-	public By Edt_TextArea(String TextArea_id) {return By.xpath("//textarea[@id='"+TextArea_id+"']");}	
+	public By Edt_TextArea(String TextArea_id) {return By.xpath("//textarea[@id='"+TextArea_id+"']");}
+	public By Edt_Email(String EmailClass) {return By.xpath("//input[contains(@class,'"+EmailClass+"')]");}
 	//Ddl
 	public By DdlOrg(String HomeClass) {return By.xpath("//select[@class='"+HomeClass+"']");}
 }
