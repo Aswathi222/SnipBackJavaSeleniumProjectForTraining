54f4b155147632304e0b60164f9e57e8381f2ed2
SsangeethaM <msangeetha9187@gmail.com>
2025-04-16 18:29:48 +0530

    PR Created for Team_07

--- src/main/java/TestData/AddUpdateDeleteDuplicateTeamGameDefaults_TestData.java
+++ src/main/java/TestData/AddUpdateDeleteDuplicateTeamGameDefaults_TestData.java
@@ -14,4 +14,9 @@ public class AddUpdateDeleteDuplicateTeamGameDefaults_TestData
 
 	//Team_06_AddUpdateDeleteDuplicateTeamGameDefaults
 	public static String Team_06_SelectedValueAdmin="demo organization";	
+
+	//Team_07_AddUpdateDeleteDuplicateTeamGameDefaults
+	public static String Team_07_SelectedValueAdmin="demo organization";
+	public static String Team_07_TeamName="TestingTeam";
+	public static String Team_07_Username="Abin Jose Francis";
 }
