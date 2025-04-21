41b1ac841aebc47f35e5e77a8ed01b70166dca98
Annu-James <annamaria1551@gmail.com>
2025-04-21 17:52:02 +0530

    SH_15_SearchGamesTeamAndMembers Issue resolved

--- src/main/java/objectRepository/SearchGameTeamAndMembers_Obj.java
+++ src/main/java/objectRepository/SearchGameTeamAndMembers_Obj.java
@@ -9,7 +9,5 @@ public class SearchGameTeamAndMembers_Obj {
 		public By Ele_MemberListNew(String memberListClass,String memberStyle,String memberId){return By.xpath("//div[@class='"+memberListClass+"']//div[@style='"+memberStyle+"']//p[@id='"+memberId+"']");}
 		public By Ele_GameFirst(String gameId,String gameClass){return By.xpath("//div[@id='"+gameId+"']//span[@class='"+gameClass+"']");}
 		public By Ele_GameSpecial(String gameId){return By.xpath("//div[@id='"+gameId+"']");}
-		public By Ele_NoMemberList(String tableClass,String memberId){return By.xpath("//div[@class='"+tableClass+"']//div[@id='"+memberId+"']");}
-		public By Ele_NoMemberData(String tableID,String memberStyle){return By.xpath("//div[@id='"+tableID+"']//div[@style='"+memberStyle+"']");}
 		public By Ele_NoMemberListData(String memberListClass,String memberStyle){return By.xpath("//div[@class='"+memberListClass+"']//div[@style='"+memberStyle+"']");}
 }
