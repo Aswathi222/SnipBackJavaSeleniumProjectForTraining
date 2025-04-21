32dfb859b90448785e1ec5556a8204178e630613
Chandanababu98 <chandanababu98@gmail.com>
2025-04-21 10:23:30 +0530

    WE_17_CreateAndAddNewMemberWithOrWithoutEmail PR Created

--- src/main/java/objectRepository/CreateAndAddNewMemberWithOrWithoutEmail_Obj.java
+++ src/main/java/objectRepository/CreateAndAddNewMemberWithOrWithoutEmail_Obj.java
@@ -8,6 +8,7 @@ public class CreateAndAddNewMemberWithOrWithoutEmail_Obj {
 	public By Btn_Team(String Team_id, String Team_text) {return By.xpath("//div[@id='"+Team_id+"']//span[text()='"+Team_text+"']");}
 	public By Btn_ThreeDots(String ThreeDots_id1, String ThreeDots_id2) {return By.xpath("//div[@id='"+ThreeDots_id1+"']//a[@id='"+ThreeDots_id2+"']");}
 	public By Btn_EditTeam(String EdtTeam_dataname) {return By.xpath("//a[@data-name='"+EdtTeam_dataname+"']");}
+	public By Btn_Close(String Close_div_id, String Close_btn_class) {return By.xpath("//div[@id='"+Close_div_id+"']//button[@class='"+Close_btn_class+"']");}
 	//Ele	
 	public By Ele_list(String Ele_list_id1, String Ele_list_id2) {return By.xpath("//div[@id='"+Ele_list_id1+"']//div[@id='"+Ele_list_id2+"']");}
 	public By Ele_SearchGame(String Edt_TeamNameClass) {return By.xpath("//input[@class='"+Edt_TeamNameClass+"']");}
