179bfec1084b3b00fec54b0d2b2b7ced7dcdb1f8
Annu-James <annamaria1551@gmail.com>
2025-04-22 10:57:48 +0530

    AI_01_AddorEditInfo Pr Created

---
+++ src/main/java/objectRepository/AddorEditInfo_Obj.java
@@ -0,0 +1,8 @@
+package objectRepository;
+
+import org.openqa.selenium.By;
+
+public class AddorEditInfo_Obj {
+	public By Ele_ThreeDots(String text,String classDots){return By.xpath("//a[normalize-space(text())='"+text+"']/../../..//div[@class='"+classDots+"']");}
+	public By Ele_ThreeDotsText(String textDots,String textDotAI){return By.xpath("//a[normalize-space(text())='"+textDots+"']/../../..//a[normalize-space(text())='"+textDotAI+"']");}
+}
