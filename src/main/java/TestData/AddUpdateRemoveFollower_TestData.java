b5040fad4889ac0d792a3035d2b0e17b880a233a
Benson Biju <94179666+LeviAckerman22@users.noreply.github.com>
2025-04-22 10:41:31 +0530

    PR CREATED FOR Follower_03_AddUpdateRemoveFollower

---
+++ src/main/java/TestData/AddUpdateRemoveFollower_TestData.java
@@ -0,0 +1,6 @@
+package TestData;
+
+public class AddUpdateRemoveFollower_TestData {
+	//Follower_03_AddUpdateRemoveFollower
+	public static String Follower_03_SelectedValue="demo organization";	
+}
