156049781c5e6ab256e9f99e4d66ef2f1844093a
encountersid <siddarthsanjay99@gmail.com>
2025-04-22 15:09:23 +0530

    PR Created for DT_03_DuplicateTeam

--- src/main/java/TestData/DuplicateTeam_TestData.java
+++ src/main/java/TestData/DuplicateTeam_TestData.java
@@ -9,4 +9,9 @@ public class DuplicateTeam_TestData
 	//DT_02_DuplicateTeam
 	public static String DT_02_OrgName="demo organization";
 	public static String DT_02_TeamName="DupTeam";
+	
+	//DT_03_DuplicateTeam
+	public static String DT_03_OrgName="demo organization";
+	public static String DT_03_TeamName="DupTeam";
+	public static String DT_03_ValidMessage="Team Name already exists";
 }
