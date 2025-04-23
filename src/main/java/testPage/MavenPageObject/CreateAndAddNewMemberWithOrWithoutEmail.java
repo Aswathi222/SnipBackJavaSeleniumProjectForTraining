5a3604988ace28375ad24d4325a5d81c64278377
Chandanababu98 <chandanababu98@gmail.com>
2025-04-23 11:04:53 +0530

    WE_23_CreateAndAddNewMemberWithOrWithoutEmail PR Created

--- src/main/java/testPage/MavenPageObject/CreateAndAddNewMemberWithOrWithoutEmail.java
+++ src/main/java/testPage/MavenPageObject/CreateAndAddNewMemberWithOrWithoutEmail.java
@@ -1337,4 +1337,53 @@ public class CreateAndAddNewMemberWithOrWithoutEmail extends BasePge {
 		base.buttonClick(LoginPageObj.Btn_SingnIn("saveMutipleUsers"));
 		asrt.assertTrue(base.isExists(LoginPageObj.Edt_AlertText("Users Added Successfully!")), "The user is not able to add new member .");	
 	}
+	//<summery>
+	//Test Case Title : To verify whether there is a option to add user without email.
+	//Automation ID : WE_23
+	//</summery>
+	public void WE_23_CreateAndAddNewMemberWithOrWithoutEmail() throws InterruptedException {	
+		Login login = new Login(driver);
+		LoginPage_Obj LoginPageObj = new LoginPage_Obj();
+		CreateAndAddNewMemberWithOrWithoutEmail_Obj CreateAndAddNewMemberWithOrWithoutEmailObj = new CreateAndAddNewMemberWithOrWithoutEmail_Obj();
+		CreateAndAddNewMemberWithOrWithoutEmail_TestData CreateAndAddNewMemberTestDataobj = new CreateAndAddNewMemberWithOrWithoutEmail_TestData();
+
+		//Step 1 : Navigate to Snipback Login page
+		//Expected : User should be able to navigate to Snipback Login page.
+		asrt.assertTrue(base.isExists(LoginPageObj.Btn_Login("Login")),"User is not able to view Snipback page");
+
+		//Step 2 : Login to Snipback
+		//Expected : User should be able to login.
+		login.loginToApplication(CommonData.UserName, CommonData.PassWord);
+		asrt.assertTrue(base.isExists(LoginPageObj.Edt_AlertMessage("GAMES")), "User is not able to login");
+
+		//Step 3 : Select any organization
+		//Expected : User should be able to select any organization.
+		base.selectorByVisibleText(CreateAndAddNewMemberWithOrWithoutEmailObj.DdlOrg("form-select select-form film-organizations"), CreateAndAddNewMemberTestDataobj.WE_23_CreateandAddnewmemberOrganization);
+		String Text = element.DropDownText(CreateAndAddNewMemberWithOrWithoutEmailObj.DdlOrg("form-select select-form film-organizations"));
+		asrt.assertEquals(Text, CreateAndAddNewMemberTestDataobj.WE_23_CreateandAddnewmemberOrganization, "User is not able to select any organization.");
+
+		//Step 4 : Switch to any Team
+		//Expected : User should be able to Switch to any Team.
+		base.setData(LoginPageObj.Edt_LoginEmail("searchTeam"), CreateAndAddNewMemberTestDataobj.WE_23_CreateandAddnewmemberTeam);
+		base.pressKey(LoginPageObj.Edt_LoginEmail("searchTeam"), "ENTER");
+		Thread.sleep(3000);
+		base.buttonClick(LoginPageObj.Edt_AlertMessage("475 Playmakers"));
+		String TeamName = base.GetText(LoginPageObj.Edt_AlertMessage("475 Playmakers"));
+		asrt.assertEquals(TeamName,CreateAndAddNewMemberTestDataobj.WE_23_CreateandAddnewmemberTeam,"User is not able to Switch to any Team.");
+
+		//Step 5 : Click three dots of that team
+		//Expected : User should be able to Click three dots of that team.
+		base.buttonClick(CreateAndAddNewMemberWithOrWithoutEmailObj.Btn_ThreeDots("my-team-content", "defaultDropdown-1"));
+		asrt.assertTrue(base.isExists(CreateAndAddNewMemberWithOrWithoutEmailObj.Ele_list("my-team-content", "teamDropdownMenu-1")), "User is not able to Click three dots of that team");
+		Thread.sleep(7000);
+
+		//Step 6 :  Select "Edit Team" option 
+		//Expected : User should be able to select "Edit Team" option.
+		base.buttonClick(CreateAndAddNewMemberWithOrWithoutEmailObj.Btn_EditTeam("475 Playmakers"));
+		asrt.assertTrue(base.isExists(CreateAndAddNewMemberWithOrWithoutEmailObj.Ele_SearchGame("form-control form-control-wrap")), "User is not able to select \"Edit Team\" option");
+
+		//Step 7 :   Observe whether there is a option as Create & Add new member without Email
+		//Expected : There should be a option to add user without mail id.
+		asrt.assertTrue(base.isExists(LoginPageObj.Edt_AlertMessage("Create & Add New Member Without Email")), "There is no option to add user without mail id.");
+	}
 }
