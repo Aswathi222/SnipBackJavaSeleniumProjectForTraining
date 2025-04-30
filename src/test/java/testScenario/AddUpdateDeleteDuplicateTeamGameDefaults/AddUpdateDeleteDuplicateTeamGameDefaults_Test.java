9593ba695470f66201cc0e8d617270e3d62b684e
SsangeethaM <msangeetha9187@gmail.com>
2025-04-30 14:57:28 +0530

    PR created for Team__27

--- src/test/java/testScenario/AddUpdateDeleteDuplicateTeamGameDefaults/AddUpdateDeleteDuplicateTeamGameDefaults_Test.java
+++ src/test/java/testScenario/AddUpdateDeleteDuplicateTeamGameDefaults/AddUpdateDeleteDuplicateTeamGameDefaults_Test.java
@@ -150,4 +150,10 @@ public class AddUpdateDeleteDuplicateTeamGameDefaults_Test extends BaseTest
 		AddUpdateDeleteDuplicateTeamGameDefaults addupdatedeleteduplicateteamgamedefaults = new AddUpdateDeleteDuplicateTeamGameDefaults(getDriver());	
 		addupdatedeleteduplicateteamgamedefaults.Team_26_AddUpdateDeleteDuplicateTeamGameDefaults();	
 	}
+	@Test()
+	public void Team_27_AddUpdateDeleteDuplicateTeamGameDefaultsTest() throws Exception 
+	{	
+		AddUpdateDeleteDuplicateTeamGameDefaults addupdatedeleteduplicateteamgamedefaults = new AddUpdateDeleteDuplicateTeamGameDefaults(getDriver());	
+		addupdatedeleteduplicateteamgamedefaults.Team_27_AddUpdateDeleteDuplicateTeamGameDefaults();	
+	}
 }
