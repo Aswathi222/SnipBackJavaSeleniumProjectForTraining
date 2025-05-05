e82003a947f3a2cf5a75ca2abdf9725e688b0cfe
Chandanababu98 <chandanababu98@gmail.com>
2025-05-05 16:15:18 +0530

    Invitelink_03_TeamInviteLink PR Created

---
+++ src/main/java/TestData/TeamInviteLink_TestData.java
@@ -0,0 +1,11 @@
+package TestData;
+
+import utilPack.BasePge;
+
+public class TeamInviteLink_TestData {
+
+	// Invitelink_03_TeamInviteLink
+	public static String Invitelink_03_TeamInviteLinkOrganization="C4-Org1 - 4262 CHILD 1";
+}
+
+
