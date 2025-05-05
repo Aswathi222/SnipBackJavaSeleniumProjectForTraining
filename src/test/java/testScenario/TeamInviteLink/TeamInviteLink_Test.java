88045eccb0c80d71f664b889a207968e29338bb0
Chandanababu98 <chandanababu98@gmail.com>
2025-05-05 10:34:17 +0530

    Invitelink_02_TeamInviteLink PR Created

--- src/test/java/testScenario/TeamInviteLink/TeamInviteLink_Test.java
+++ src/test/java/testScenario/TeamInviteLink/TeamInviteLink_Test.java
@@ -12,5 +12,11 @@ public class TeamInviteLink_Test extends BaseTest
 	{	
 		TeamInviteLink TeamInviteLink = new TeamInviteLink(getDriver());
 		TeamInviteLink.Invitelink_01_TeamInviteLink();			
+	}
+	@Test()
+	public void Invitelink_02_TeamInviteLinkTest() throws Exception 
+	{	
+		TeamInviteLink TeamInviteLink = new TeamInviteLink(getDriver());
+		TeamInviteLink.Invitelink_02_TeamInviteLink();			
 	} 
 }
