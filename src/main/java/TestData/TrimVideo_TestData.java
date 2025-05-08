6227f42c0344831581b1189186448aa0f8dec7e6
Annu-James <annamaria1551@gmail.com>
2025-05-08 18:01:25 +0530

    TV_02_TrimVideo PR Created

--- src/main/java/TestData/TrimVideo_TestData.java
+++ src/main/java/TestData/TrimVideo_TestData.java
@@ -7,5 +7,8 @@ public class TrimVideo_TestData {
 	//TV_01_TrimVideo
 	public static String TV_01_TrimVideo="T-144";
 	public static String TV_01_UploadedVideo="QA";
-	public static String TV_01_PausedVideo="s9";	
+	public static String TV_01_PausedVideo="s9";
+	
+	//TV_02_TrimVideo
+	public static String TV_02_TrimVideo="T-12";
 }
