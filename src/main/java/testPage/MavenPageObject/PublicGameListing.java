dac988599322a66e110a512c40f17d6e1c6c171b
Hasnaek <hasnahassanek@gmail.com>
2025-05-29 10:42:53 +0530

    PR Created for TC01_PublicGameListing

---
+++ src/main/java/testPage/MavenPageObject/PublicGameListing.java
@@ -0,0 +1,37 @@
+package testPage.MavenPageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import TestData.CommonData;
+import TestData.SnipBackLogin_TestData;
+import objectRepository.GameListing_Obj;
+import objectRepository.LoginPage_Obj;
+import objectRepository.Registration_Obj;
+import utilPack.BasePge;
+//import utilPack.BaseTest;
+//import basePackage.BasePge;
+
+public class PublicGameListing extends BasePge {
+
+	BasePge base;
+	Assert asrt;
+
+	public PublicGameListing(WebDriver driver) {    //WebDriver used to interact with browser
+		super(driver);                              //here we are calling the driver given in the basepge class(ie;chromedriver)
+		base = new BasePge(driver);                 //base act as a variable
+	}
+	//<summary>
+	//Test Case Title:"Verify that the user should be able to navigates to the  Snipback website after entering the URL
+	//Automation ID: Game Listing_01
+	//</summary>
+	public void TC01_PublicGameListing() {
+		//Step1: Enter snipback URL
+		//Expected Result: The user should be able to navigates to the  Snipback website after entering the URL
+		LoginPage_Obj home = new LoginPage_Obj();
+		asrt.assertTrue(base.isExists(home.Ele_SnipBackHomePageLogo("light-logo")),"Unable to navigate to Snipback website");
+		
+	}
+}
---
+++ src/test/java/testScenario/PublicGameListing/PublicGameListing_Test.java
@@ -0,0 +1,22 @@
+package testScenario.PublicGameListing;
+
+import org.testng.annotations.Test;
+
+import testPage.MavenPageObject.PublicGameListing;
+import testPage.MavenPageObject.SnipBackLogin;
+import utilPack.BaseTest;
+
+
+public class PublicGameListing_Test extends BaseTest 
+
+{	
+
+	
+	@Test()
+	public void TC01_PublicGameListingTest() throws Exception 
+	{	
+		PublicGameListing GameLogin = new PublicGameListing(getDriver());      
+		GameLogin.TC01_PublicGameListing();   //calling our method using object of our created class
+		
+	}
+}
