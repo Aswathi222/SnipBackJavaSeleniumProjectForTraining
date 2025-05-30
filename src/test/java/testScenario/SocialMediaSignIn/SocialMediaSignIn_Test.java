e22086612d5537c5f887bc5654c04d085f15096a
Rajeev-1990 <sjrajeev@gmail.com>
2025-05-30 15:40:32 +0530

    TC04_SocialMediaSignIn() - SignIn With Apple Account - PR Created.

--- src/test/java/testScenario/SocialMediaSignIn/SocialMediaSignIn_Test.java
+++ src/test/java/testScenario/SocialMediaSignIn/SocialMediaSignIn_Test.java
@@ -11,4 +11,11 @@ public class SocialMediaSignIn_Test extends BaseTest
 		SocialMediaSignIn SignIn = new SocialMediaSignIn(getDriver());
 		SignIn.TC01_SocialMediaSignIn();	
 	}
+	
+	@Test()
+	public void TC04_SocialMediaSignIn() throws Exception 
+	{	
+		SocialMediaSignIn SignIn = new SocialMediaSignIn(getDriver());
+		SignIn.TC04_SocialMediaSignIn();		
+	}
 }
