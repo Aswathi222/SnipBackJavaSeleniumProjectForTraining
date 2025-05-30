7a2095f4dd8d140a28b93e4f2cc17a94a2962133
Rajeev-1990 <sjrajeev@gmail.com>
2025-05-30 10:33:49 +0530

    TC01_SocialMediaSignIn - SignIn With Google - PR Created.

---
+++ src/test/java/testScenario/SocialMediaSignIn/SocialMediaSignIn_Test.java
@@ -0,0 +1,15 @@
+package testScenario.SocialMediaSignIn;
+import org.testng.annotations.Test;
+import testPage.MavenPageObject.SnipBackLogin;
+import testPage.MavenPageObject.SocialMediaSignIn;
+import utilPack.BaseTest;
+
+public class SocialMediaSignIn_Test extends BaseTest 
+{	
+	@Test()
+	public void TC01_SocialMediaSignIn() throws Exception 
+	{	
+		SocialMediaSignIn SignIn = new SocialMediaSignIn(getDriver());
+		SignIn.TC01_SocialMediaSignIn();		
+	}
+}
