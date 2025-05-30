647cac70d4bf4256839a464331420a1b0ea8d2c1
Rajeev-1990 <sjrajeev@gmail.com>
2025-05-30 17:13:38 +0530

    TC01_CreateAddNewMember - Checking Option to Add user With Email - PR Created.

---
+++ src/main/java/TestData/CreateAddNewMember_TestData.java
@@ -0,0 +1,12 @@
+package TestData;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+public class CreateAddNewMember_TestData
+{
+	//GenericData
+	public static String TeamName = "CreateORAddUserName";
+	public static String Email = "test123@gmail.com";
+	public static String ActualErrorMessage="Please enter the valid email, the invalid characters are &/\\#,+()$~%.^':*?<>{}\"";
+	public static String InvalidEmail = "asdasdasdasdsad";
+}
---
+++ src/test/java/testScenario/CreateAddNewMember/CreateAddNewMember_Test.java
@@ -0,0 +1,16 @@
+package testScenario.CreateAddNewMember;
+import org.testng.annotations.Test;
+import testPage.MavenPageObject.CreateAddNewMember;
+import testPage.MavenPageObject.SnipBackLogin;
+import testPage.MavenPageObject.SocialMediaSignIn;
+import utilPack.BaseTest;
+
+public class CreateAddNewMember_Test extends BaseTest
+{	
+	@Test()
+	public void TC01_CreateAddNewMember() throws Exception
+	{	
+		CreateAddNewMember CreateNew = new CreateAddNewMember(getDriver());
+		CreateNew.TC01_CreateAddNewMember();	
+	}
+}
